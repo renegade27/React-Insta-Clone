a0e22bcc9e5162a7a07130fceb4e3b1bf1402358
Zero <jrr043@latech.edu>
2019-03-17 23:09:52 -0500

    Search bar and functionality added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -9,12 +9,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data : []
+      search : '',
+      data : [],
+      filteredData: []
     }
   }
   
   componentDidMount() {
-    setTimeout(() => { this.setState(prevState => { return { data: dummyData }})},1000);
+    setTimeout(() => { this.setState(prevState => { return { data: dummyData, filteredData: dummyData }})},1000);
+  }
+
+  searchHandler = e => {
+    this.setState({filteredData : this.state.data.filter(post => post.username.includes(e.target.value)), search : e.target.value});
   }
 
   render() {
@@ -22,12 +28,22 @@ class App extends Component {
         <div className="page-wrapper">
           <nav>
             <i className="fab fa-instagram"></i>
+            <p>Instagram</p>
+            <input
+              type="text"
+              placeholder="Search"
+              value={this.state.search}
+              onChange={this.searchHandler}
+            />
+            <div className="nav-icons">
+              <i className="far fa-compass"></i>
+              <i className="far fa-heart"></i>
+              <i className="far fa-user"></i>
+            </div>
           </nav>
-          <>
-            {this.state.data.length === 0 ? (<p>Loading...</p>) :
-            (this.state.data.map( post => { return <div className="post"> <PostContainer post={post} /> </div> } 
-            ))};
-          </>
+          {this.state.filteredData.length === 0 ? (<p>Loading...</p>) :
+          (this.state.filteredData.map( post => { return <div className="post"> <PostContainer post={post} /> </div> } 
+          ))};
         </div>
       );
   }
